04529f1a5bad5f978456c7790f7593ccea512773
begly <service.alex@begly.co.uk>
2014-11-11 21:05:47 +0000

    Add rotation to cube

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ document.body.appendChild(renderer.domElement);
 
 // Add cube
 var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial( {color: 0x00FF00} );
+var material = new THREE.MeshBasicMaterial( {color: '#00FF00'} );
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
@@ -18,6 +18,8 @@ camera.position.z = 5;
 
 function render() {
     requestAnimationFrame(render);
+    cube.rotation.x += 0.1;
+    cube.rotation.y += 0.1;
     renderer.render(scene, camera);
 }
 
